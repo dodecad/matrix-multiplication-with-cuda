0da48c4cba41d67670a84293d88ccc53ea267e5f
Alexander <dodecad@outlook.com>
2018-01-05 16:57:22 +0300

    GPU implementation of the parallel algorithm

--- main.cpp
+++ main.cpp
@@ -1,24 +1,32 @@
 #include <cstdlib>
 #include <iostream>
 #include <time.h>
+#include <cuda_runtime_api.h>
 #include <stdio.h>
 #include "mmult.h"
 
 int main()
 {
     const int m = BLOCK_SIZE * 12, n = BLOCK_SIZE * 18, k = BLOCK_SIZE * 24;
-    float * a, * b, * c, * c_verify;
+    float * a, * b, * c, * a_gpu, * b_gpu, * c_gpu, * c_verify;
 
     a = new float[m * n];
     b = new float[n * k];
     c = new float[m * k];
     c_verify = new float[m * k];
 
+    cudaMalloc((void **) &a_gpu, m * n * sizeof *a_gpu);
+    cudaMalloc((void **) &b_gpu, n * k * sizeof *b_gpu);
+    cudaMalloc((void **) &c_gpu, m * k * sizeof *c_gpu);
+
     for (int i = 0; i < m * n; ++i)
         a[i] = float(rand()) / RAND_MAX;
 
     for (int i = 0; i < n * k; ++i)
         b[i] = float(rand()) / RAND_MAX;
+
+    cudaMemcpy(a_gpu, a, m * n * sizeof *a_gpu, cudaMemcpyHostToDevice);
+    cudaMemcpy(b_gpu, b, n * k * sizeof *b_gpu, cudaMemcpyHostToDevice);
 
     float sTime;
     clock_t start, finish;
@@ -30,6 +38,15 @@ int main()
     sTime = (float)(finish - start) / CLOCKS_PER_SEC;
     printf("Run time on CPU: %lf sec", sTime);
 
+    start = clock();
+    mmult_gpu(m, n, k, a_gpu, b_gpu, c_gpu);
+    cudaThreadSynchronize();
+    cudaMemcpy(c_verify, c_gpu, m * k * sizeof *c_gpu, cudaMemcpyDeviceToHost);
+    finish = clock();
+
+    sTime = (float)(finish - start) / CLOCKS_PER_SEC;
+    printf("Run time on GPU: %lf sec",sTime);
+
     float difference = 0;
 
     for (int i = 0; i < m * k; ++i)
@@ -39,6 +56,11 @@ int main()
         std::cout << "Test passed.\n";
     else
         std::cout << "Test failed (diff = " << difference << ").\n";
+
+    cudaMemset(c_gpu, 0, m * k * sizeof *c_gpu);
+    cudaFree(c_gpu);
+    cudaFree(b_gpu);
+    cudaFree(a_gpu);
     
     delete [] c_verify;
     delete [] c;
--- mmult.h
+++ mmult.h
@@ -3,11 +3,12 @@
 
 #define BLOCK_SIZE 16
 
-// Matrix multiplication function
+// Matrix multiplication functions
 // c = a * b
 // a: m x n
 // b: n x k
 // c: m x k
 void mmult(int m, int n, int k, const float * a, const float * b, float * c);
+void mmult_gpu(int m, int n, int k, const float * a, const float * b, float * c);
 
 #endif
